d09b1411ac8d78473ecba6869d8a595bbe86f41b
nkreimer <nkreimer@NKREIMER-PC>
2016-06-02 14:05:40 -0400

    updated roll method

--- CSharp/BowlingGame/BowlingGame/Game.cs
+++ CSharp/BowlingGame/BowlingGame/Game.cs
@@ -8,13 +8,16 @@ namespace BowlingGame
 {
     public class Game
     {
+        private int score = 0;
+
         public void Roll(int pins)
         {
+            score += pins;
         }
 
         public int Score()
         {
-            return 0;
+            return score;
         }
 
     }
