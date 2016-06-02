e57ff6de8ecd17666902939ef85c2446ecc2ef91
nkreimer <nkreimer@NKREIMER-PC>
2016-06-02 09:06:03 -0400

    TestOne pass TestTwo fail

---
+++ CSharp/PrimeFactors/PrimeFactors.Test/PrimeFactorsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeFactors.Test
+{
+    [TestClass]
+    public class PrimeFactorsTest
+    {
+        private List<int> list(int[] ints = null)
+        {
+            List<int> list = new List<int>();
+            if (ints != null)
+            {
+                foreach (int i in ints)
+                    list.Add(i);
+            }
+            return list;
+        }
+
+        [TestMethod]
+        public void TestOne()
+        {
+            CollectionAssert.AreEqual(list(), PrimeFactors.Generate(1));
+        }
+
+        [TestMethod]
+        public void TestTwo()
+        {
+            CollectionAssert.AreEqual(list(new int[2]), PrimeFactors.Generate(2));
+        }
+    }
+}
