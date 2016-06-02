34f12d6d39aacb4a650fda6116d676a8f9788e7c
nkreimer <nkreimer@NKREIMER-PC>
2016-06-02 15:55:02 -0400

    refactored test and TestOneSpare

--- CSharp/BowlingGame/BowlingGame.Test/BowlingGameTest.cs
+++ CSharp/BowlingGame/BowlingGame.Test/BowlingGameTest.cs
@@ -8,6 +8,12 @@ namespace BowlingGame.Test
     {
         private Game g;
 
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+                g.Roll(pins);
+        }
+
         [TestInitialize]
         public void Setup()
         {
@@ -17,19 +23,27 @@ namespace BowlingGame.Test
         [TestMethod]
         public void TestGutterGame()
         {
-            for (int i = 0; i < 20; i++)
-                g.Roll(0);
-
+            RollMany(20, 0);
             Assert.AreEqual(0, g.Score());
         }
 
         [TestMethod]
         public void TestAllOnes()
         {
-            for (int i = 0; i< 20; i++)
-                g.Roll(1);
+            RollMany(20, 1);
             Assert.AreEqual(20, g.Score());
         }
+
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            g.Roll(5);
+            g.Roll(5); // spare
+            g.Roll(3);
+            RollMany(17,0);
+            Assert.AreEqual(16,g.Score());
+        }
+
     }
 }
 
