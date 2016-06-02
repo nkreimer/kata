49a2df06798abc98bf3640e7bbd38044c6bab364
nkreimer <nkreimer@NKREIMER-PC>
2016-06-02 13:29:17 -0400

    Setup Test

--- CSharp/BowlingGame/BowlingGame.Test/BowlingGameTest.cs
+++ CSharp/BowlingGame/BowlingGame.Test/BowlingGameTest.cs
@@ -7,7 +7,7 @@ namespace BowlingGame.Test
     public class BowlingGameTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGutterGame()
         {
         }
     }
