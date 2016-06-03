bec04bc639003feb2868fed53ff743d089dd0444
nkreimer <nkreimer@NKREIMER-PC>
2016-06-03 12:11:26 -0400

    Second pass

--- CSharp/FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ CSharp/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -15,6 +15,8 @@ namespace FizzBuzz
             //{
             if (i % 3 == 0)
                 return "Fizz";
+            else if (i % 5 == 0)
+                return "Buzz";
             else
                 return i.ToString();
             //}
