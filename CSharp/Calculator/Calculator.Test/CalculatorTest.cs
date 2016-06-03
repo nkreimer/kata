1e0a4edba47dde4cf8d1126be2601046260600df
nkreimer <nkreimer@NKREIMER-PC>
2016-06-03 12:53:33 -0400

    New Solution

---
+++ CSharp/Calculator/Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
