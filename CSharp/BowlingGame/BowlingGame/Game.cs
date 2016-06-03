5b9df19533ed6a8c031a9f4d3ac8ed6c4b8e27a6
nkreimer <nkreimer@NKREIMER-PC>
2016-06-03 07:41:02 -0400

    Refactor score method

--- CSharp/BowlingGame/BowlingGame/Game.cs
+++ CSharp/BowlingGame/BowlingGame/Game.cs
@@ -19,8 +19,13 @@ namespace BowlingGame
         public int Score()
         {
             int score = 0;
-            for (int i = 0; i < rolls.Length; i++)
-                score += rolls[i];
+            int i = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                score += rolls[i] + rolls[i + 1];
+                i += 2;
+            }
+
 
             return score;
         }
