53d5886580f4f45d701606633a62bd109f6d2aff
nkreimer <nkreimer@NKREIMER-PC>
2016-06-03 10:50:58 -0400

    New solution with renamed test project

--- CSharp/FizzBuzz/FizzBuzz.Test/Properties/AssemblyInfo.cs
+++ CSharp/FizzBuzz/FizzBuzz.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FizzBuzz.Test")]
+[assembly: AssemblyTitle("FizzBuzzTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FizzBuzz.Test")]
+[assembly: AssemblyProduct("FizzBuzzTest")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f4dc7628-1c3d-4319-9f9e-cfe5975cabd7")]
+[assembly: Guid("58f024ce-333b-4bed-a036-9794869f8d3f")]
 
 // Version information for an assembly consists of the following four values:
 //
--- CSharp/FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ CSharp/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-    public class Class1
+    public class FizzBuzz
     {
     }
 }
