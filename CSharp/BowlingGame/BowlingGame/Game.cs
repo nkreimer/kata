e76f5fa0e73d66295f2f5a6a35a44e5d9a6a478f
nkreimer <nkreimer@NKREIMER-PC>
2016-06-03 08:14:08 -0400

    Refactor for strike

--- CSharp/BowlingGame/BowlingGame/Game.cs
+++ CSharp/BowlingGame/BowlingGame/Game.cs
@@ -24,26 +24,39 @@ namespace BowlingGame
             {
                 if (rolls[frameIndex] == 10) // strike
                 {
-                    score += 10 +
-                             rolls[frameIndex + 1] +
-                             rolls[frameIndex + 2];
+                    score += 10 + StrikeBonus(frameIndex);
                     frameIndex++;
                 }
                 else if (IsSpare(frameIndex))
                 {
-                    score += 10 + rolls[frameIndex + 2];
+                    score += 10 + SpareBonus(frameIndex);
                     frameIndex += 2;
                 }
                 else
                 {
-                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    score += SumOfBallsInFrame(frameIndex);
                     frameIndex += 2;
                 }
-
             }
 
             return score;
         }
+
+        private int SumOfBallsInFrame(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 2];
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+
 
         private bool IsSpare(int frameIndex)
         {
