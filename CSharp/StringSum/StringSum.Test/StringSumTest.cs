36949d1d04c1e87f6f9de266c6906a5bee8541b8
nkreimer <nkreimer@NKREIMER-PC>
2016-06-08 12:29:35 -0400

    New solution

---
+++ CSharp/StringSum/StringSum.Test/StringSumTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringSum.Test
+{
+    [TestClass]
+    public class StringSumTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
