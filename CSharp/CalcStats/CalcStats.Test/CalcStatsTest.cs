2d47050a925fd654f595811ff9176de432a7f686
nkreimer <nkreimer@NKREIMER-PC>
2016-06-09 15:07:52 -0400

    Impleted functions and test

--- CSharp/CalcStats/CalcStats.Test/CalcStatsTest.cs
+++ CSharp/CalcStats/CalcStats.Test/CalcStatsTest.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace CalcStats.Test
 {
 	[TestClass]
 	public class CalcStatsTest
 	{
-        	[TestMethod]
-        	public void TestMethod1()
-        	{
-        	}
-	}
+        [TestMethod]
+        public void NumberStats_WithListOfNumbersReturnMinValue()
+        {
+            var numbers = new List<int>() { 6, 9, 15, -2, 92, 11 };
+            
+            Assert.AreEqual("-2", CalcStats.NumberStats(numbers, CalcStats.CalcStatKeys.Minimum));
+        }
+
+        [TestMethod]
+        public void NumberStats_WithListOfNumbersReturnMaxValue()
+        {
+            var numbers = new List<int>() { 6, 9, 15, -2, 92, 11 };
+
+            Assert.AreEqual("92", CalcStats.NumberStats(numbers, CalcStats.CalcStatKeys.Maximum));
+        }
+
+        [TestMethod]
+        public void NumberStats_WithListOfNumbersReturnNumberOfItems()
+        {
+            var numbers = new List<int>() { 6, 9, 15, -2, 92, 11 };
+
+            Assert.AreEqual("6", CalcStats.NumberStats(numbers, CalcStats.CalcStatKeys.ElementCount));
+        }
+
+        [TestMethod]
+        public void NumberStats_WithListOfNumbersReturnAverageValue()
+        {
+            var numbers = new List<int>() { 6, 9, 15, -2, 92, 11 };
+
+            Assert.AreEqual("21", CalcStats.NumberStats(numbers, CalcStats.CalcStatKeys.Average));
+        }
+    }
 }
--- CSharp/CalcStats/CalcStats/CalcStats.cs
+++ CSharp/CalcStats/CalcStats/CalcStats.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace CalcStats
 {
-    public class CalcStats
+    public static class CalcStats
     {
+        public enum CalcStatKeys
+        {
+            Minimum = 1, Maximum = 2, ElementCount = 3, Average = 4
+        }
+
+        public static string NumberStats(IList<int> numbers, CalcStatKeys keys)
+        {
+            switch(keys)
+            {
+                case CalcStatKeys.Minimum:
+                    return GetMinValue(numbers);
+                case CalcStatKeys.Maximum:
+                    return GetMaxValue(numbers);
+                case CalcStatKeys.ElementCount:
+                    return GetElementCount(numbers);
+                case CalcStatKeys.Average:
+                    return GetAverageValue(numbers);              
+            }
+            return null;
+        }
+
+        private static string GetMinValue(IList<int> numbers)
+        {
+            return numbers.Min().ToString();
+        }
+
+        private static string GetMaxValue(IList<int> numbers)
+        {
+            return numbers.Max().ToString();
+        }
+
+        private static string GetElementCount(IList<int> numbers)
+        {
+            return numbers.Count().ToString();
+        }
+
+        private static string GetAverageValue(IList<int> numbers)
+        {
+            return (numbers.Sum() / numbers.Count()).ToString();
+        }
     }
 }
