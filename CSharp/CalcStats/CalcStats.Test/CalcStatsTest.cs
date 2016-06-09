7e9496206c1efdbcff18e17ae9659cfed4515a61
nkreimer <nkreimer@NKREIMER-PC>
2016-06-09 14:27:50 -0400

    New Solution

---
+++ CSharp/CalcStats/CalcStats.Test/CalcStatsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcStats.Test
+{
+	[TestClass]
+	public class CalcStatsTest
+	{
+        	[TestMethod]
+        	public void TestMethod1()
+        	{
+        	}
+	}
+}
