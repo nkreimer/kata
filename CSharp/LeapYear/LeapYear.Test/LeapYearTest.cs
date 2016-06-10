34106aeca5adc12775998db86b905fc67654293c
nkreimer <nkreimer@NKREIMER-PC>
2016-06-10 08:26:12 -0400

    LeapYear test

--- CSharp/LeapYear/LeapYear.Test/LeapYearTest.cs
+++ CSharp/LeapYear/LeapYear.Test/LeapYearTest.cs
@@ -7,8 +7,17 @@ namespace LeapYear.Test
     public class LeapYearTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsLeapYear_WithLeapYearReturnTrue()
         {
+            Assert.AreEqual(true, LeapYear.IsLeapYear("1996"));
+            Assert.AreEqual(true, LeapYear.IsLeapYear("2000"));
+        }
+
+        [TestMethod]
+        public void IsLeapYear_WithNonLeapYearReturnFalse()
+        {
+            Assert.AreEqual(false, LeapYear.IsLeapYear("2001"));
+            Assert.AreEqual(false, LeapYear.IsLeapYear("1900"));
         }
     }
 }
--- CSharp/LeapYear/LeapYear/LeapYear.cs
+++ CSharp/LeapYear/LeapYear/LeapYear.cs
@@ -8,9 +8,18 @@ namespace LeapYear
 {
     public static class LeapYear
     {
-        public static Boolean IsLeapYear(string date)
+        public static Boolean IsLeapYear(string year)
         {
+            /*
+                A leap year is defined as one that is divisible by 4,
+            but is not otherwise divisible by 100 unless it is
+            also divisble by 400.
+            */
+            var intYear = int.Parse(year);
 
+            if (intYear % 4 == 0)
+                if (intYear % 100 > 0 || intYear % 400 == 0)
+                        return true;
             return false;
         }
     }
