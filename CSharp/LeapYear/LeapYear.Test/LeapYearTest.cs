1295e76c9b1091a650d6cae9f2f84ddef772b4ac
nkreimer <nkreimer@NKREIMER-PC>
2016-06-10 07:31:54 -0400

    new solution

---
+++ CSharp/LeapYear/LeapYear.Test/LeapYearTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeapYear.Test
+{
+    [TestClass]
+    public class LeapYearTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ CSharp/LeapYear/LeapYear/LeapYear.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    public static class LeapYear
+    {
+        public static Boolean IsLeapYear(string date)
+        {
+
+            return false;
+        }
+    }
+}
